0c29793df328322cdb10831c8b2809bd88eaad4a
Jeremy Rempel <jeremyrempel@gmail.com>
2020-06-19 09:59:00 -0400

    cleanup

--- app/src/main/java/com/github/jeremyrempel/strictmodetest/MyApplication.kt
+++ app/src/main/java/com/github/jeremyrempel/strictmodetest/MyApplication.kt
@@ -3,13 +3,7 @@ package com.github.jeremyrempel.strictmodetest
 import android.app.Application
 import android.os.Build
 import android.os.StrictMode
-import android.os.StrictMode.VmPolicy
-import android.os.strictmode.Violation
-import android.util.Log
-import java.lang.RuntimeException
-import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
-import java.util.concurrent.ThreadPoolExecutor
 
 
 class MyApplication : Application() {
@@ -32,12 +26,8 @@ class MyApplication : Application() {
                                     val methodName = it.methodName
 
                                     val clazz = Class.forName(className)
-
                                     val methods = clazz.methods.filter { m -> m.name == methodName }
-
-                                    isIgnore = methods.filter { m ->
-                                        m.isAnnotationPresent(IgnoreStrictMode::class.java)
-                                    }.isNotEmpty()
+                                    isIgnore = methods.any { m -> m.isAnnotationPresent(IgnoreStrictMode::class.java) }
                                 }
                             }
 
@@ -49,6 +39,13 @@ class MyApplication : Application() {
                                 throw RuntimeException("strict mode violation, add to ignore or resolve")
                             }
                         })
+                    .build()
+            )
+        } else {
+            StrictMode.setThreadPolicy(
+                StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
                     .build()
             )
         }
